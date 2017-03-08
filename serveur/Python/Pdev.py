42247fbbeae9da551057c00deecdc0db3eb09b6a
AntoineMoutou <moutou.antoine@hotmail.fr>
2017-03-08 15:30:41 +0100

    Amélioration de la lisibilité des prints

--- serveur/Python/Pdev.py
+++ serveur/Python/Pdev.py
@@ -22,28 +22,19 @@ def main(shapefile):
     
     # Nombres de sommets
     nb_nodes = nx.number_of_nodes(G)
-    print(nb_nodes)
+    print("nb_nodes", nb_nodes)
     # Nombres d'arcs
     nb_edges = nx.number_of_edges(G)
-    print(nb_edges)
-    # Centre
-    center = nx.center(G)
-    print(center)
+    print("nb_edges", nb_edges)
     # Diamètre
     diameter = nx.diameter(G)
-    print(diameter)
-    # Eccentricité
-    eccentricity = nx.eccentricity(G)
-    print(eccentricity)
-    # Périphérie
-    periphery = nx.periphery(G)
-    print(periphery)
+    print("diameter", diameter)
     # Rayon
     radius = nx.radius(G)
-    print(radius)
+    print("radius", radius)
     # Nombres de composants connexes
     number_connected_components = nx.number_connected_components(G)
-    print(number_connected_components)
+    print("number_connected_components", number_connected_components)
     # Longueur totale (somme des longueurs des arcs)
     # Trafic total (somme des trafics/poids des arcs)
     
@@ -52,7 +43,7 @@ def main(shapefile):
     # Indice de détour (Longueur du graphe rapportée à la longueur du réseau)
     # Densité (Longueur du graphe rapporté à la surface de l'espace étudié)
     density = nx.density(G)
-    print(density)
+    print("density", density)
     # Indice "pi" (Longueur du graphe rapportée à la longueur du diamètre)
     # Indice "eta" (Longueur du graphe rapportée au nombre d'arcs)
     # Indice "theta" (Traffic total rapporté au nombre de sommets)
@@ -72,7 +63,7 @@ def main(shapefile):
     # Transitivité (average clustering coefficient)
     # Longueur moyenne des plus courts chemin
     average_shortest_path_length = nx.average_shortest_path_length(G)
-    print(average_shortest_path_length)
+    print("average_shortest_path_length", average_shortest_path_length)
     # Indice oligopolistique (Rich-club coefficient)
     #rich_club_coefficient = nx.rich_club_coefficient(G)
     #print(rich_club_coefficient)
