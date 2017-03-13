f8376f9057a46b51c49b3e69a96c98b84f71d9d0
Ogus <ogustin.gagnon@gmail.com>
2017-03-13 13:57:24 +0100

    Ajout de la documentation pour JSDoc

--- client/dist/script.js
+++ client/dist/script.js
@@ -1,3 +1,10 @@
+/**
+* @fileOverview Script côté client pour gérer l'upload des shp, et l'interface utilisateur
+* @author Augustin Gagnon <augustin.gagnon@ensg.eu>
+* @requires Leaflet {@link http://leafletjs.com/}
+* @requires JSZip {@link https://stuk.github.io/jszip/}
+* @requires shapefile-js {@link https://github.com/calvinmetcalf/shapefile-js}
+*/
 window.onload = function(){
   var  zip_file, shp_file;
   var busy = true;
@@ -10,7 +17,9 @@ window.onload = function(){
   var file_name = document.getElementById("file_name");
   var map_button = document.getElementById("map_button");
 
-  // initialize all event listener
+  /**
+  * Initialise les écouteurs d'évènements de la page
+  */
   function init_listener(){
     busy = false;
     zip = new JSZip();
@@ -26,6 +35,9 @@ window.onload = function(){
     map_button.addEventListener("change", set_background, false);
   }
 
+  /**
+  * Initialise la carte Leaflet
+  */
   function init_map(){
     map = L.map('shp_preview').setView([48.853, 2.345], 9);
     map_raster = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -36,7 +48,9 @@ window.onload = function(){
     set_background();
   }
 
-
+  /**
+  * Ajoute ou supprime la carte en fond de page
+  */
   function set_background(){
     if(map_button.checked){
       map.addLayer(map_raster);
@@ -46,6 +60,11 @@ window.onload = function(){
     }
   }
 
+  /**
+  * Écouteur d'évènement associé au glisser & déposer d'un fichier
+  * @listens document.drop
+  * @param e {event} e - l'évènement déclenché par le glisser & déposer
+  */
   function document_drop(e){
     if(busy){ return; }
     e.preventDefault();
@@ -54,6 +73,11 @@ window.onload = function(){
     handle_file(file);
   }
 
+  /**
+  * Écouteur d'évènement associé au clique sur le bouton de chargement d'un fichier
+  * @listens upload_button.change
+  * @param {event} e - l'évènement déclenché par le chargement d'un fichier
+  */
   function document_select(e){
     if(busy){ return; }
 
@@ -61,11 +85,15 @@ window.onload = function(){
     handle_file(file);
   }
 
+  /**
+  * Vérifie le contenu du fichier ZIP, et supprime les fichiers supplémentaires
+  * @param {file} file - Le fichier chargé par l'utilisateur
+  */
   function handle_file(file){
     JSZip.loadAsync(file).then(function(zip){
       var type, idx, file_types = ['shp', 'dbf', 'prj'];
 
-      // check up and clean the zip file
+      // parcours le ZIP pour vérifier chaque fichier
       zip.forEach(function(relativePath, zipEntry) {
         type = zipEntry.name.split(".").pop().toLowerCase();
         idx = file_types.indexOf(type);
@@ -74,8 +102,12 @@ window.onload = function(){
         else{ zip.remove(relativePath); }
       });
 
-      // zip file has correct files in it
+      // si le ZIP contient tout les fichiers nécéssaires
       if(file_types.length < 1 || (file_types.length == 1 && file_types[0] == 'prj')){
+        zip_file = zip;
+        shp_file = zip.file(/.shp$/i)[0];
+
+        // affiche le nom à l'écran
         var div = document.querySelector("#file_wrap .content");
         div.innerHTML = "<div class='file_name'>\
         <p class='name'>"+file.name.split(".")[0]+"</p>\
@@ -83,17 +115,7 @@ window.onload = function(){
         </div>";
         document.getElementsByClassName("close")[0].addEventListener('click', reset_file);
 
-        zip_file = zip;
-        shp_file = zip.file(/.shp$/i)[0];
-
-        // shp_file.async("arraybuffer")
-        // .then(function success(content) {
-        //   upload_file(content);
-        // }, function error(e) {
-        //   throw e;
-        // });
-
-        // read zip as array buffer to display it
+        // Lecture du ZIP au format ArrayBuffer pour l'affichage
         zip_file.generateAsync({type:"arraybuffer"})
         .then(function success(content) {
           display_zip(content);
@@ -101,6 +123,7 @@ window.onload = function(){
           throw e;
         });
 
+        // Lecture du ZIP au format UInt8Array pour l'envoi sur le serveur Node
         zip_file.generateAsync({type:"uint8array"})
         .then(function success(content) {
           upload_zip(content);
@@ -116,27 +139,38 @@ window.onload = function(){
     });
   }
 
+  /**
+  * Convertit le fichier SHP au format Geojson, et l'affiche sur la carte.
+  * Cette fonction utilise le module shapefile-js pour la conversion
+  * @param {ArrayBuffer} buffer - Le buffer de données correspondant au fichier SHP
+  */
   function display_zip(buffer){
     shp(buffer).then(function(geojson){
       if(map_vector){ map.removeLayer(map_vector) }
       map_vector = L.geoJSON(geojson);
+
       map.addLayer(map_vector);
       map.fitBounds(map_vector.getBounds());
     });
   }
 
+  /**
+  * Envoi le ZIP contenant le fichier SHP au serveur avec une requete AJAX.
+  * Le dossier ZIP est encapsulé dans un objet FormData pour être récupéré par le serveur.
+  * @param {UInt8Array} data - Le chaîne encodé correspondant aux données du dossier ZIP
+  */
   function upload_zip(data){
-    console.log(data);
-    console.log(data.length);
+    // console.log(data);
+    // console.log(data.length);
     var form = new FormData();
     form.append('zip',data);
     form.append('option',123456);
-    console.log(form);
+    // console.log(form);
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://127.0.0.1:8080/", true);
 
-    // keep track of progression for progress bar
+    // Les écouteurs d'évènements permettent d'animer une barre de chargement
     xhr.onprogress = function(e){
       if(e.lengthComputable) { }
     };
@@ -152,8 +186,13 @@ window.onload = function(){
     xhr.send(form);
   }
 
-
-  function reset_file(){    // reset file
+  /**
+  * Réinitialise les éléments suivants sur la page Web:
+  * - la couche vecteur affiché sur la carte Leaflet
+  * - Le dossier ZIP enregistré dans une variable
+  * - Le texte indiquant le dossier ZIP chargé
+  */
+  function reset_file(){
     map.removeLayer(map_vector);
     zip_file = undefined;
     shp_file = undefined;
@@ -161,6 +200,11 @@ window.onload = function(){
     document.querySelector("#file_wrap .content").innerHTML = "<p class='file_name'>No file uploaded</p>";
   }
 
+  /**
+  Affiche un texte d'alerte au premier plan
+  * @param {String} text - Le texte à afficher à l'écran
+  * @param {Integer} timeout - La durée d'affichage de l'alerte
+  */
   function alert(text,timeout){
     var div = document.createElement("div");
     div.innerHTML = "<p>"+text+"</p>";
@@ -177,5 +221,4 @@ window.onload = function(){
 
   init_map();
   init_listener();
-  // reset_file();
 };
