6deeeba4d8df3a118dcdec7ed476a05e5f26dd22
AntoineMoutou <moutou.antoine@hotmail.fr>
2017-03-14 04:05:03 +0100

    MAJ pour être au bon format
    Il reste encore des fonctions à coder et à commenter

--- serveur/Python/Pdev.py
+++ serveur/Python/Pdev.py
@@ -3,69 +3,128 @@ import skimage
 import matplotlib
 import scipy
 import networkx as nx
+import sys
 
-print ("numpy =" ,np.__version__)
-print ("skimage =", skimage.__version__)
-print ("matplotlib = ",matplotlib.__version__)
-print ("scipy =",scipy.__version__)
-print ("networkx =" ,nx.__version__)
+shapefile = sys.argv[1]
+#shapefile = "donnees//1854_emprise.shp"
 
-shapefile = "donnees//1854_emprise.shp"
+nomMethode = sys.argv[2]
+#bomMethode = "degree"
 
-def main(shapefile):
-
+def main(shapefile, nomMethode):
+    """
+    Fonction permettant de calculer un attribut particulier à un shp en utilisant la théorie des graphes.
+    Les attributs sont : 
+        - basics 
+    """
+    
+    # Transformation du shapefile en graphe
     G = nx.read_shp(shapefile)
     
+    # On rend le graphe non-orienté
     G = G.to_undirected()
     
-    # Taille du réseau
+    if nomMethode == "basics":
+        fun_basics(G)
+        
+    elif nomMethode == "diameter":
+        fun_diameter(G)
+        
+    elif nomMethode == "radius":
+        fun_radius(G)
+        
+    elif nomMethode == "number_connected_component":
+        fun_number_connected_component(G)
+        
+    elif nomMethode == "density":
+        fun_density(G)
+        
+    elif nomMethode == "average_shortest_path_length":
+        fun_average_shortest_path_length(G)
+        
+    else :
+        print("nom de méthode non défini")
+        
+        
+# Taille du réseau
+
+
+def fun_basics(G) :
     
     # Nombres de sommets
     nb_nodes = nx.number_of_nodes(G)
     print("nb_nodes", nb_nodes)
     # Nombres d'arcs
     nb_edges = nx.number_of_edges(G)
-    print("nb_edges", nb_edges)
+    
+    s = '{"basics":{"nb_nodes" : ' + str(nb_nodes) + ', "nb_edges" : ' + str(nb_edges) + '}}'
+    print(s)
+
+def fun_diameter(G) :
+    
     # Diamètre
     diameter = nx.diameter(G)
-    print("diameter", diameter)
+    
+    s = '{"diameter" : ' + str(diameter) + '}'
+    print(s)
+    
+def fun_radius(G) :
+    
     # Rayon
     radius = nx.radius(G)
-    print("radius", radius)
+    
+    s = '{"radius" : ' + str(radius) + '}'
+    print(s)
+    
+def fun_number_connected_component(G) :
+    
     # Nombres de composants connexes
     number_connected_components = nx.number_connected_components(G)
-    print("number_connected_components", number_connected_components)
-    # Longueur totale (somme des longueurs des arcs)
-    # Trafic total (somme des trafics/poids des arcs)
     
-    # Organisation du réseau
+    s = '{"number_connected_components" : ' + str(number_connected_components) + '}'
+    print(s)
+
+# Longueur totale (somme des longueurs des arcs)
+# Trafic total (somme des trafics/poids des arcs)
+
+
+# Organisation du réseau
+
+
+# Indice de détour (Longueur du graphe rapportée à la longueur du réseau)
+def fun_density(G) : 
     
-    # Indice de détour (Longueur du graphe rapportée à la longueur du réseau)
     # Densité (Longueur du graphe rapporté à la surface de l'espace étudié)
     density = nx.density(G)
-    print("density", density)
-    # Indice "pi" (Longueur du graphe rapportée à la longueur du diamètre)
-    # Indice "eta" (Longueur du graphe rapportée au nombre d'arcs)
-    # Indice "theta" (Traffic total rapporté au nombre de sommets)
+    s = '{"density" : ' + str(density) + '}'
+    print(s)
     
-    # Mesures de la structure (Théorie des Graphes)
-    
-    # Nombre cyclomatique (nombre maximum de cycles indépendants)
-    # Indice alpha (nombre de cycles sur nombres maximums de cycle possible)
-    # Indice beta (nombre d'arcs sur nombre de sommets)
-    # Indice gamma (nombre d'arcs sur nombre maximum d'arcs possible)
-    # Centralité (Somme des centralités individuelles des noeuds)
-    
-    # Mesures de la structure (complexité)
-    
-    # Indice de hiérarchie (Scale-free)
-    # Transitivité (Clustering coefficient)
-    # Transitivité (average clustering coefficient)
+# Indice "pi" (Longueur du graphe rapportée à la longueur du diamètre)
+# Indice "eta" (Longueur du graphe rapportée au nombre d'arcs)
+# Indice "theta" (Traffic total rapporté au nombre de sommets)
+
+# Mesures de la structure (Théorie des Graphes)
+
+# Nombre cyclomatique (nombre maximum de cycles indépendants)
+# Indice alpha (nombre de cycles sur nombres maximums de cycle possible)
+# Indice beta (nombre d'arcs sur nombre de sommets)
+# Indice gamma (nombre d'arcs sur nombre maximum d'arcs possible)
+# Centralité (Somme des centralités individuelles des noeuds)
+
+# Mesures de la structure (complexité)
+
+# Indice de hiérarchie (Scale-free)
+# Transitivité (Clustering coefficient)
+# Transitivité (average clustering coefficient)
+# Longueur moyenne des plus courts chemin
+def fun_average_shortest_path_length(G) : 
     # Longueur moyenne des plus courts chemin
     average_shortest_path_length = nx.average_shortest_path_length(G)
-    print("average_shortest_path_length", average_shortest_path_length)
-    # Indice oligopolistique (Rich-club coefficient)
-    #rich_club_coefficient = nx.rich_club_coefficient(G)
-    #print(rich_club_coefficient)
+    s = '{"average_shortest_path_length" : ' + str(average_shortest_path_length) + '}'
+    print(s)
 
-main(shapefile)
+# Indice oligopolistique (Rich-club coefficient)
+#rich_club_coefficient = nx.rich_club_coefficient(G)
+#print(rich_club_coefficient)
+
+main(shapefile, nomMethode)
