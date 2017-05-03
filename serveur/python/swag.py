164dc5e6963cf0810d7dc2ca8d3f0328231318cc
AntoineMoutou <moutou.antoine@hotmail.fr>
2017-05-03 16:33:11 +0200

    Add files via upload

--- serveur/python/swag.py
+++ serveur/python/swag.py
@@ -8,6 +8,7 @@ import time
 
 
 shapefile = sys.argv[1]
+
 #shapefile = "D://pc//Documents//ENSG//IT2//Projet Développement//donnees//1854_emprise.shp"
 #shapefile = "D://pc//Documents//ENSG//IT2//Projet Développement//donnees//1871_L93_utf8_emprise.shp"
 #shapefile = "D://pc//Documents//ENSG//IT2//Projet Développement//donnees//andriveau_l93_utf8_corr.shp"
@@ -15,23 +16,24 @@ shapefile = sys.argv[1]
 
 
 nomMethode = sys.argv[2]
-#nomMethode = "rich_club_coefficient"
+
+#nomMethode = "basics"
 
 
 
 def set_shp(shapefile):
-
+    
     #Ouvre un shp et récupère ses noms d'attributs
     source = ogr.Open(shapefile, update=True)
     layer = source.GetLayer()
     layer_defn = layer.GetLayerDefn()
     field_names = [layer_defn.GetFieldDefn(i).GetName() for i in range(layer_defn.GetFieldCount())]
-
+    
     #Ajoute un nouvel attribut
     if not("LENGTH" in field_names):
         new_field = ogr.FieldDefn("LENGTH", ogr.OFTReal)
         layer.CreateField(new_field)
-
+    
         #Remplir le nouvel attribut
         for feature in layer:
             geom = feature.GetGeometryRef()
@@ -45,87 +47,7 @@ def set_shp(shapefile):
 
 
 
-def main(shapefile, nomMethode):
-    """
-    Fonction permettant de calculer un attribut particulier à un shp en utilisant la théorie des graphes.
-    Les attributs sont :
-        - basics
-        - diameter
-        - radius
-        - number_connected_component
-        - density
-        - average_shortest_path_length
-        - index_beta_gamma
-        - index_pi_eta_theta
-    """
 
-    # Prétraitement du shp (création et calcul de la colonne LENGTH)
-    set_shp(shapefile)
-
-    # Transformation du shapefile en graphe
-    G2 = nx.read_shp(shapefile)
-
-    # On rend le graphe non-orienté
-    G = G2.to_undirected()
-    # print("coool",G.is_directed())
-
-    #nx.draw_networkx(G)
-
-    #print(G.edges(data=True)[0])
-
-    if nomMethode == "basics":
-        fun_basics(G)
-
-    elif nomMethode == "diameter":
-        fun_diameter(G)
-
-    elif nomMethode == "radius":
-        fun_radius(G)
-
-    elif nomMethode == "number_connected_component":
-        fun_number_connected_component(G)
-
-    elif nomMethode == "density":
-        fun_density(G)
-
-    elif nomMethode == "average_shortest_path_length":
-        fun_average_shortest_path_length(G,"LENGTH")
-
-    elif nomMethode == "index_beta_gamma":
-        fun_index_beta_gamma(G)
-
-    elif nomMethode == "index_pi_eta_theta":
-        fun_index_pi_eta_theta(G)
-
-    elif nomMethode == "degree_centrality":
-        fun_degree_centrality(G)
-
-    elif nomMethode == "closeness_centrality":
-        fun_closeness_centrality(G,"LENGTH")
-
-    elif nomMethode == "betweenness_centrality":
-        fun_betweenness_centrality(G,"LENGTH")
-
-    elif nomMethode == "eigenvector_centrality":
-        fun_eigenvector_centrality(G,"LENGTH")
-
-    elif nomMethode == "katz_centrality":
-        fun_katz_centrality(G,"LENGTH")
-
-    elif nomMethode == "flow_hierarchy":
-        fun_flow_hierarchy(G,"LENGTH")
-
-    elif nomMethode == "clustering_coefficient":
-        fun_clustering_coefficient(G,"LENGTH")
-
-    elif nomMethode == "average_clustering_coefficient":
-        fun_average_clustering_coefficient(G,"LENGTH")
-
-    elif nomMethode == "rich_club_coefficient":
-        fun_rich_club_coefficient(G)
-
-    else :
-        print("nom de méthode non défini")
 
 
 #/////////////////////////////////////////////////////////////////////
@@ -137,10 +59,10 @@ def fun_basics(G) :
 
     # Nombres de sommets
     nb_nodes = nx.number_of_nodes(G)
-
+    
     # Nombres d'arcs
     nb_edges = nx.number_of_edges(G)
-
+    
     # Longueur totale (somme des longueurs des arcs)
     total_length = 0
     list_edges = G.edges(data=True)
@@ -175,15 +97,10 @@ def fun_number_connected_component(G) :
     print(s)
 
 
-# Trafic total (somme des trafics/poids des arcs)
-
-
 #/////////////////////////////////////////////////////////////////////
 # Organisation du réseau
 #/////////////////////////////////////////////////////////////////////
 
-
-# Indice de détour (Longueur du graphe rapportée à la longueur du réseau)
 
 def fun_density(G) :
 
@@ -192,70 +109,90 @@ def fun_density(G) :
     s = '{"density" : ' + str(density) + '}'
     print(s)
 
-def fun_index_pi_eta_theta(G) :
-
-    # Indice "pi" (Longueur du graphe rapportée à la longueur du diamètre)
-    pi = 0
-
-    # Indice "eta" (Longueur du graphe rapportée au nombre d'arcs)
+def fun_index_pi_eta(G) :
+    
+    e = nx.number_of_edges(G)
+    
+    d = nx.diameter(G)
+    
     total_length = 0
-    nb_edges = 0
     list_edges = G.edges(data=True)
     for edge in list_edges:
         total_length += edge[-1]["LENGTH"]
-        nb_edges += 1
-    eta = total_length / nb_edges
-
-    # Indice "theta" (Traffic total rapporté au nombre de sommets)
-    theta = 0
-
-    s = '{"index_pi_eta_theta":{"pi" : ' + str(pi) + ', "eta" : ' + str(eta) + ', "theta" : ' + str(theta) + '}}'
+        
+    # Indice "pi" (Longueur du graphe rapportée à la longueur du diamètre)
+    pi = total_length / d
+    
+    # Indice "eta" (Longueur du graphe rapportée au nombre d'arcs)
+    eta = total_length / e
+    
+    s = '{"index_pi_eta":{"pi" : ' + str(pi) + ', "eta" : ' + str(eta) + '}}'
     print(s)
-
+    
+    
 #/////////////////////////////////////////////////////////////////////
 # Mesures de la structure (Théorie des Graphes)
 #/////////////////////////////////////////////////////////////////////
 
 
-# Nombre cyclomatique (nombre maximum de cycles indépendants)
-
-def fun_index_beta_gamma(G) :
-
-    # Indice beta (nombre d'arcs sur nombre de sommets)
-    beta = nx.number_of_edges(G) / nx.number_of_nodes(G)
-
-    # Indice gamma (nombre d'arcs sur nombre maximum d'arcs possible)
-    gamma = nx.number_of_edges(G) / (nx.number_of_nodes(G) - 2) * 3
-
-    s = '{"index_beta_gamma":{"beta" : ' + str(beta) + ', "gamma" : ' + str(gamma) + '}}'
+def fun_cyclomatic_number(G):
+    
+    # Nombre cyclomatique (nombre maximum de cycles indépendants)
+    v = nx.number_of_nodes(G)
+    e = nx.number_of_edges(G)
+    p = nx.number_connected_components(G)
+    
+    u = e - v + p
+    
+    s = '{"cyclomatic_number":' + str(u) + '}'
     print(s)
 
+def fun_index_alpha_beta_gamma(G) :
+    
+    v = nx.number_of_nodes(G)
+    e = nx.number_of_edges(G)
+    p = nx.number_connected_components(G)
+    
+    u = e - v + p
+    
+    # Indice alpha (nombre de cycles sur le nombre de cycles maximum)
+    alpha = u / (2*v - 5)
+    
+    # Indice beta (nombre d'arcs sur nombre de sommets)
+    beta = e / v
+    
+    # Indice gamma (nombre d'arcs sur nombre maximum d'arcs possible)
+    gamma = e / (v - 2) / 3
+    
+    s = '{"index_alpha_beta_gamma":{"alpha" : ' + str(alpha) + ', "beta" : ' + str(beta) + ', "gamma" : ' + str(gamma) + '}}'
+    print(s)
+    
 def fun_degree_centrality(G) :
-
+         
     # Centralité de degré
     s = nx.degree_centrality(G)
     print(s)
-
+ 
 def fun_closeness_centrality(G,weight) :
-
+    
     # Centralité de proximité avec la prise en compte des distances
     s = nx.current_flow_closeness_centrality(G,weight)
     print(s)
 
 def fun_betweenness_centrality(G,weight) :
-
+    
     # Centralité d'intermédiarité
     s = nx.current_flow_betweenness_centrality(G,weight)
     print(s)
 
 def fun_eigenvector_centrality(G,weight) :
-
+    
     # Centralité de vecteur propre
     s = nx.eigenvector_centrality_numpy(G,weight)
     print(s)
 
 def fun_katz_centrality(G,weight) :
-
+    
     # Centralité de Katz
     s = nx.katz_centrality_numpy(G,0.1,1.0,True,weight)
     print(s)
@@ -266,22 +203,15 @@ def fun_katz_centrality(G,weight) :
 #/////////////////////////////////////////////////////////////////////
 
 
-def fun_flow_hierarchy(G,weight):
-
-    # Indice de hiérarchie (Scale-free)
-    flow_hierarchy = nx.flow_hierarchy(G,weight)
-    s = '{"flow_hierarchy" : ' + str(flow_hierarchy) + '}'
-    print(s)
-
 def fun_clustering_coefficient(G,weight) :
-
+    
     # Transitivité (Clustering coefficient)
     clustering_coefficient = nx.clustering(G,None,weight)
     s = clustering_coefficient
     print(s)
-
+    
 def fun_average_clustering_coefficient(G,weight):
-
+    
     # Transitivité (average clustering coefficient)
     average_clustering_coefficient = nx.average_clustering(G,None,weight)
     s = average_clustering_coefficient
@@ -293,14 +223,118 @@ def fun_average_shortest_path_length(G,weight=None) :
     s = '{"average_shortest_path_length" : ' + str(average_shortest_path_length) + '}'
     print(s)
 
-def fun_rich_club_coefficient(G):
-    # Indice oligopolistique (Rich-club coefficient)
-    rich_club_coefficient = nx.rich_club_coefficient(G)
-    print(rich_club_coefficient)
 
-# toc = time.time()
+#/////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////
+
+# Main
+
+#/////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////
+
+
+def main(shapefile, nomMethode):
+    """
+    Fonction permettant de calculer un attribut particulier à un shp en utilisant la théorie des graphes.
+    
+    Les attributs sont :
+        
+        - basics
+        
+        - diameter
+        - radius
+        - number_connected_component
+        
+        - density
+        - index_alpha_beta_gamma
+        - index_pi_eta_theta
+        
+        - degree_centrality
+        - closeness_centrality
+        - betweenness_centrality
+        - eigenvector_centrality
+        - katz_centrality
+        
+        - average_shortest_path_length
+        - clustering_coefficient
+        - average_clustering_coefficient
+    """
+    
+    # Prétraitement du shp (création et calcul de la colonne LENGTH)
+    set_shp(shapefile)
+    
+    # Transformation du shapefile en graphe
+    G2 = nx.read_shp(shapefile)
+
+    # On rend le graphe non-orienté
+    G = G2.to_undirected()
+    # print("coool",G.is_directed())
+
+    #nx.draw_networkx(G)
+    
+    #print(G.edges(data=True)[0])
+
+    if nomMethode == "basics":
+        fun_basics(G)
+
+    elif nomMethode == "diameter":
+        fun_diameter(G)
+
+    elif nomMethode == "radius":
+        fun_radius(G)
+
+    elif nomMethode == "number_connected_component":
+        fun_number_connected_component(G)
+
+    elif nomMethode == "density":
+        fun_density(G)
+
+    elif nomMethode == "index_alpha_beta_gamma":
+        fun_index_alpha_beta_gamma(G)
+        
+    elif nomMethode == "index_pi_eta_theta":
+        fun_index_pi_eta(G)
+        
+    elif nomMethode == "degree_centrality":
+        fun_degree_centrality(G)
+    
+    elif nomMethode == "closeness_centrality":
+        fun_closeness_centrality(G,"LENGTH")
+    
+    elif nomMethode == "betweenness_centrality":
+        fun_betweenness_centrality(G,"LENGTH")
+        
+    elif nomMethode == "eigenvector_centrality":
+        fun_eigenvector_centrality(G,"LENGTH")
+        
+    elif nomMethode == "katz_centrality":
+        fun_katz_centrality(G,"LENGTH")
+        
+    elif nomMethode == "clustering_coefficient":
+        fun_clustering_coefficient(G,"LENGTH")
+        
+    elif nomMethode == "average_clustering_coefficient":
+        fun_average_clustering_coefficient(G,"LENGTH")
+        
+    elif nomMethode == "cyclomatic_number":
+        fun_cyclomatic_number(G)
+    
+    elif nomMethode == "average_shortest_path_length":
+        fun_average_shortest_path_length(G,"LENGTH")
+        
+    else :
+        print("nom de méthode non défini")
+
+
+#/////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////
+
+
+toc = time.time()
 
 main(shapefile, nomMethode)
 
-# tic = time.time()
-# print(tic-toc)
+tic = time.time()
+#print("time" + str(tic-toc))
